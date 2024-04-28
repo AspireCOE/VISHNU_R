a7dc8b39704d11500ded5610afcb12d0d76efb03
Vishnu R <vishnulrk@gmail.com>
2024-04-28 18:04:02 +0530

    Created Inheritance

--- JAVA OOPS CONCEPTS/Inheritance.java
+++ JAVA OOPS CONCEPTS/Inheritance.java
@@ -1,3 +1,23 @@
+class Animal{
+    void eat(){
+        System.out.println("Eating...");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Barking...");
+    }
+}
+class WildDog extends Dog{
+    void sleep(){
+        System.out.println("Sleeping");
+    }
+}
 public class Inheritance {
-    
+    public static void main(String args[]){
+        WildDog bow=new WildDog();
+        bow.eat();
+        bow.bark();
+        bow.sleep();
+    }
 }
